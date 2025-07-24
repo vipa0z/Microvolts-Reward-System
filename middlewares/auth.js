79c8b4d693d038c4eebcd7176b81b480ed07712b
vipa0z <abdelrahmanmagd3660@gmail.com>
2025-07-25 01:18:04 +0300

    add auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+
+
+
+function adminAuthMiddleware(req, res, next) {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Missing or invalid token' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, JWT_ADMIN_SECRET);
+    req.user = decoded; 
+    next(); 
+  } catch (err) {
+    return res.status(403).json({ message: 'Invalid or expired token' });
+  }
+}
+
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Missing or invalid token' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded; 
+    next(); 
+  } catch (err) {
+    return res.status(403).json({ message: 'Invalid or expired token' });
+  }
+}
+
+module.exports = {authMiddleware,adminAuthMiddleware};
+
+
+
